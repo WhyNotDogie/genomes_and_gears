61f4d7915e52d06e7559bf648fca62d72b6c6b6e
WhyNotDogie <112395091+WhyNotDogie@users.noreply.github.com>
2025-05-26 00:29:39 -0700

    i freaking did it

--- neoforge/src/main/java/dev/dogie/examplemod/ExampleMod.java
+++ neoforge/src/main/java/dev/dogie/examplemod/ExampleMod.java
@@ -16,5 +16,6 @@ public class ExampleMod {
         // Use NeoForge to bootstrap the Common mod.
         Constants.LOG.info("Hello NeoForge world!");
         CommonClass.init();
+        AllItems.REGISTRATE.registerEventListeners(eventBus);
     }
 }
