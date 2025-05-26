30696b0570c7a29f82aaa2c327fbdc7149c19f26
WhyNotDogie <112395091+WhyNotDogie@users.noreply.github.com>
2025-05-26 10:23:13 -0700

    Remove test items

--- common/src/main/java/dev/dogie/genomes_and_gears/AllItems.java
+++ common/src/main/java/dev/dogie/genomes_and_gears/AllItems.java
@@ -7,14 +7,9 @@ import net.minecraft.world.item.Item;
 public class AllItems {
     public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(Constants.MOD_ID);
 
-    public static final ItemEntry<Item> TESTITEM = REGISTRATE.item("test", Item::new)
-            .lang("test")
-            .onRegister(i -> Constants.LOG.info("This never happens"))
-            .register();
+    //-
 
     // Load this class
-
     public static void register() {
-        Constants.LOG.info("This happens");
     }
 }
