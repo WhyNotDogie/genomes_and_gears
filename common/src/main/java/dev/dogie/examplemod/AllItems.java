ae9b4d9f720dd1ad2d9746ae8179eaa21d673546
WhyNotDogie <112395091+WhyNotDogie@users.noreply.github.com>
2025-05-25 23:42:41 -0700

    it doesnt work yet but who cares because it compiles

---
+++ common/src/main/java/dev/dogie/examplemod/AllItems.java
@@ -0,0 +1,20 @@
+package dev.dogie.examplemod;
+
+import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.tterrag.registrate.util.entry.ItemEntry;
+import net.minecraft.world.item.Item;
+
+public class AllItems {
+    public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(Constants.MOD_ID);
+
+    public static final ItemEntry<Item> TESTITEM = REGISTRATE.item("test", Item::new)
+            .lang("test")
+            .onRegister(i -> Constants.LOG.info("This never happens"))
+            .register();
+
+    // Load this class
+
+    public static void register() {
+        Constants.LOG.info("This happens");
+    }
+}
