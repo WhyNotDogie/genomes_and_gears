26466487c00b49ed3729c86915896f4d36e1e673
WhyNotDogie <112395091+WhyNotDogie@users.noreply.github.com>
2025-07-27 13:50:22 +0200

    this might die real quick idk

--- common/src/main/java/dev/dogie/genomes_and_gears/Constants.java
+++ common/src/main/java/dev/dogie/genomes_and_gears/Constants.java
@@ -9,4 +9,8 @@ public class Constants {
 	public static final String MOD_ID = "genomes_and_gears";
 	public static final String MOD_NAME = "Create: Genomes and Gears";
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+
+	public static ResourceLocation asloc(String id) {
+		return ResourceLocation.fromNamespaceAndPath(MOD_ID, id);
+	}
 }
---
+++ common/src/main/java/dev/dogie/genomes_and_gears/content/tools/Syringe.java
@@ -0,0 +1,36 @@
+package dev.dogie.genomes_and_gears.content.tools;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+
+public class Syringe extends Item {
+    public Syringe(Properties properties) {
+//        new Item(properties).interactLivingEntity();
+        super(properties);
+    }
+
+    @Override
+    @NotNull
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+        ItemStack itemstack = player.getItemInHand(usedHand);
+
+        return InteractionResultHolder.consume(itemstack);
+    }
+
+    @Override
+    public InteractionResult interactLivingEntity(ItemStack stack, Player player, LivingEntity target, InteractionHand hand) {
+        if (!player.level().isClientSide) {
+
+        }
+
+        return InteractionResult.CONSUME;
+    }
+}
